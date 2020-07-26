39d3abec666f1363f541ce2288471ec750efdb28
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-07-26 15:20:57 -0400

    mmm gooodyes yes
    userscript header, icon, better error messages, bigscreen detection, sound

--- Low-Res.user.js
+++ Low-Res.user.js
@@ -1,17 +1,22 @@
 // ==UserScript==
 // @name         Low-Res boxcritters
-// @namespace    http://tampermonkey.net/
-// @version      very yes
 // @description  yes blocky blur
-// @run-at       document-end
 // @author       SArpnt
+// @version      mmm gooodyes yes
+// @namespace    https://boxcrittersmods.ga/authors/sarpnt/
+// @homepage     https://boxcrittersmods.ga/mods/low-res/
+// @updateURL    https://github.com/SArpnt/Low-Res/raw/master/Low-Res.user.js
+// @downloadURL  https://github.com/SArpnt/Low-Res/raw/master/Low-Res.user.js
+// @supportURL   https://github.com/SArpnt/Low-Res/issues
+// @icon         https://github.com/SArpnt/Low-Res/raw/master/icon16.png
+// @run-at       document-end
+// @grant        none
 // @match        https://boxcritters.com/play/
 // @match        https://boxcritters.com/play/?*
 // @match        https://boxcritters.com/play/#*
 // @match        https://boxcritters.com/play/index.html
 // @match        https://boxcritters.com/play/index.html?*
 // @match        https://boxcritters.com/play/index.html#*
-// @grant        none
 // ==/UserScript==
 
 (function () {
@@ -21,6 +26,9 @@
 	function fool(txt) {
 		let youFool = document.createElement('span');
 		youFool.innerText = txt;
+		youFool.style.fontSize = '20pt';
+		youFool.style.fontWeight = 'bold';
+		youFool.style.fontFamily = "'Comic Sans MS', cursive, sans-serif";
 		canvas.insertAdjacentElement('afterend', youFool);
 		canvas.style.display = 'none';
 	}
@@ -31,6 +39,10 @@
 you blongus
 you absolute utter kerplongus
 you can't use low-res and hi-res at the same time`);
+		else if (cardboard.mods && cardboard.mods.bigScreen)
+			fool(`screen is too big
+extra large pixel machine broke
+understandable have a nice day`);
 		else {
 			let resize = function () {
 				canvas.width = 100;
@@ -43,6 +55,13 @@ you can't use low-res and hi-res at the same time`);
 			};
 			window.addEventListener('resize', resize);
 			createjs.Ticker.framerate = 8;
+			createjs.Sound.on("fileload", function () {
+				createjs.Sound.play("music", { loop: -1, volume: .9, delay: 5, });
+				createjs.Sound.play("music", { loop: -1, volume: .8, delay: 80, });
+				createjs.Sound.play("music", { loop: -1, volume: .6, delay: 160, });
+				createjs.Sound.play("music", { loop: -1, volume: .4, delay: 240, });
+				createjs.Sound.play("music", { loop: -1, volume: .6, delay: 480, });
+			});
 			resize();
 		}
 	}
